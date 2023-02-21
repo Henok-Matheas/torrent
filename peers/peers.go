6d70e19898ecf536f783bb6275938bb2f60aa585
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2023-02-21 22:09:02 +0300

    multiple peers working

--- peers/peers.go
+++ peers/peers.go
@@ -1,7 +1,6 @@
 package peers
 
 import (
-	"encoding/binary"
 	"fmt"
 	"net"
 	"strconv"
@@ -16,26 +15,26 @@ type Peer struct {
 // Unmarshal parses peer IP addresses and ports from a buffer
 func Unmarshal(peersBin []byte) ([]Peer, error) {
 	const peerSize = 6 // 4 for IP, 2 for port
-	numPeers := len(peersBin) / peerSize
-	// numPeers := 1
+	// numPeers := len(peersBin) / peerSize
+	numPeers := 1
 	if len(peersBin)%peerSize != 0 {
 		err := fmt.Errorf("received malformed peers")
 		return nil, err
 	}
 	peer := make([]byte, 4)
 	// 10.6.250.226
-	peer[0] = 10
-	peer[1] = 6
-	peer[2] = 250
-	peer[3] = 131
+	peer[0] = 127
+	peer[1] = 0
+	peer[2] = 0
+	peer[3] = 1
 
 	peers := make([]Peer, numPeers)
 	for i := 0; i < numPeers; i++ {
-		offset := i * peerSize
-		peers[i].IP = net.IP(peersBin[offset : offset+4])
-		peers[i].Port = binary.BigEndian.Uint16([]byte(peersBin[offset+4 : offset+6]))
-		// peers[i].IP = net.IP(peer)
-		// peers[i].Port = 8080
+		// offset := i * peerSize
+		// peers[i].IP = net.IP(peersBin[offset : offset+4])
+		// peers[i].Port = binary.BigEndian.Uint16([]byte(peersBin[offset+4 : offset+6]))
+		peers[i].IP = net.IP(peer)
+		peers[i].Port = 8080
 		// fmt.Println(peers[i].IP.String(), strconv.Itoa(int(peers[i].Port)))
 	}
 
